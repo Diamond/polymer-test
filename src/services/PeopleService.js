182e1df8f3bf5c3c33d00f138461c765b7c22cdd
Brandon Richey <diamondgfx@gmail.com>
2017-01-19 23:51:15 -0500

    Made it function and look exactly like my create-react-app equivalent

--- src/services/PeopleService.js
+++ src/services/PeopleService.js
@@ -1,12 +1,24 @@
+const makeName = () => {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i=0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
+
 const PeopleService = {
   index: () => {
     const data = ['Jim', 'Sally', 'Bender'];
     return {
       then(callback) {
-        setTimeout(() => callback(data), 5000);
-      }
+        setTimeout(() => callback(data), 0);
+      },
     };
   },
+  makeName: () => makeName(),
 };
 
 export default PeopleService;
